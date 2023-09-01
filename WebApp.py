7fc77f64e2ef1f7395a5857312e758b6b362d224
Zachary Loftin <zachloftin94@gmail.com>
2023-09-01 17:46:47 -0400

    Checked for missing values and added histogram in web

--- WebApp.py
+++ WebApp.py
@@ -1,10 +1,21 @@
 import pandas as pd
 import streamlit as st
+import plotly.express as px
 import plotly as pl
+import numpy as np
+import scipy as sp
+import seaborn as sns
+from PIL import Image
 import os
-
-#csv_file_path = os.path.join(os.path.dirname(__file__), 'vehicles_us.csv')
 
 vehicle_data = pd.read_csv('vehicles_us.csv')
 
-print(vehicle_data.head(20))
+st.title('Vehicle Data Analysis')
+
+#st.sidebar.header("Settings")
+
+#bins = st.sidebar.slider("Number of Bins", 1, 100, 20)
+
+histo = px.histogram(vehicle_data, x='model')
+
+st.plotly_chart(histo)
